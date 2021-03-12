ea01f23611b2c6600c03a3c1f413ca670eba626c
AdityaGattu <gattuaditya@gmail.com>
2021-03-12 17:50:02 +0530

    Day 5 lab1 completed

--- Day5/Fridayun-JavaLab/src/Theory/AbstractClassDemo.java
+++ Day5/Fridayun-JavaLab/src/Theory/AbstractClassDemo.java
@@ -1,6 +1,6 @@
 package Theory;
 
-/An abstract class is a class which is used to represent a common noun
+//An abstract class is a class which is used to represent a common noun
 //All commons nouns are by default abstract
 //Abstract class represents kind of relationship
 //Helps to create classification or part whole hierarchy
@@ -9,7 +9,7 @@ package Theory;
 //Because abstracts don't  exists in real life.
 //for example - can u see Paint,car,bus,table,chair etc - they are abstract
 //meaning of abstract is overview
-package day5;
+
 public class AbstractClassDemo {
 	public static void main(String[] args) {
 		PaintBrush brush=new PaintBrush();
@@ -38,12 +38,11 @@ class PinkPaint extends Paint{
 	
 }
 /*
-*  with the help of inheritance we created a hierarchy.
-*  we made the paintbrush reusable as an object
-*  we eliminated if-else-condition
-*  How to eliminate the if-else-if
-*  a. convert the condition to classes.
-*  b. Group them uner a hierarchy (inheritance)
-*  c. Create a associaiton (relation) between the using (PaintBrush) class and Hierarchial classifier class (Paint)
+ *  with the help of inheritance we created a hierarchy.
+ *  we made the paintbrush reusable as an object
+ *  we eliminated if-else-condition
+ *  How to eliminate the if-else-if
+ *  a. convert the condition to classes.
+ *  b. Group them uner a hierarchy (inheritance)
+ *  c. Create a associaiton (relation) between the using (PaintBrush) class and Hierarchial classifier class (Paint)
 */
-
