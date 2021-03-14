7aec8e8923175cae92e6716cb907d258eb4d3944
AdityaGattu <gattuaditya@gmail.com>
2021-03-14 20:25:26 +0530

    Friday lab4 completed

--- Day5/Fridayun-JavaLab/src/lab4/q33.java
+++ Day5/Fridayun-JavaLab/src/lab4/q33.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class q33 {
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
+		Scanner s= new Scanner(System.in);
 		int n1=s.nextInt();
 		int n2=s.nextInt();
 		
