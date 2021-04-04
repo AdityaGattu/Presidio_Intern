800d1236945eba61f295f8d8852a855d1683c16a
AdityaGattu <gattuaditya@gmail.com>
2021-04-05 01:02:15 +0530

    assignment done

--- Day19/DBMS/src/day19/InvoiceDAO.java
+++ Day19/DBMS/src/day19/InvoiceDAO.java
@@ -1,5 +1,13 @@
 package day19;
 
-public class InvoiceDAO {
+import java.util.*;
 
+public interface InvoiceDAO {
+	
+	public InvoiceDTO getIvoice(int invid);
+	public List<InvoiceDTO> getAllInvoice();
+	public int createInvoice(InvoiceDTO i);
+	public int updateInvoice(InvoiceDTO i);
+	public int deleteInvoice(InvoiceDTO i);
+	public int deleteInvoiceById(int invid);
 }
